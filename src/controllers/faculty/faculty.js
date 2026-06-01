14941946c6e1cbec15588af8d066b5f72af8ac03
Nayade de la Cruz <71477253+nayadelacruz@users.noreply.github.com>
2026-06-01 13:41:32 -0700

    Revert "Main"

--- src/controllers/faculty/faculty.js
+++ src/controllers/faculty/faculty.js
@@ -1,28 +1,31 @@
-import { getFacultyBySlug, getSortedFaculty } from '../../models/faculty/faculty.js';
+import { getFacultyById, getSortedFaculty} from '../../models/faculty/faculty.js';
 
-const facultyListPage = async (req, res) => {
-    const validSortOptions = ['name', 'department', 'title'];
-    const sortBy = validSortOptions.includes(req.query.sort) ? req.query.sort : 'department';
-    const facultyList = await getSortedFaculty(sortBy);
+const facultyListPage = (req, res) => {
+    
+    const sorting = ['name', 'department', 'title'];
+    const sortBy = req.query.sort || 'name';
+    const faculty = getSortedFaculty(sortBy);
+
     res.render('faculty/list', {
         title: 'Faculty Directory',
-        faculty: facultyList,
+        faculty : faculty,
         currentSort: sortBy
     });
 };
 
-const facultyDetailPage = async (req, res, next) => {
-    const facultySlug = req.params.facultySlug;
-    const facultyMember = await getFacultyBySlug(facultySlug);
-    if (Object.keys(facultyMember).length === 0) {
-        const err = new Error(`Faculty member ${facultySlug} not found`);
+const facultyDetailPage = (req, res, next) => {
+    const facultyId = req.params.facultyId;
+    const facultyMember = getFacultyById(facultyId);
+
+    if (!facultyMember) {
+        const err = new Error(`Faculty member ${facultyId} not found`);
         err.status = 404;
         return next(err);
-    }
+    }    
     res.render('faculty/detail', {
-        title: `${facultyMember.name} - Faculty Profile`,
-        faculty: facultyMember
-    });
+        title: `${facultyMember.name}`,
+        facultyMember: facultyMember
+    }); 
 };
 
 export { facultyListPage, facultyDetailPage};
